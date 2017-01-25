64a5b98d8d0f4919b522f05e0bf5be7903e452d2
Anthony Falcon <funksoulninja@Anthonys-MBP.PK5001Z>
2017-01-25 02:52:01 -0800

    Add gulp build and watch tasks.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,17 +4,38 @@ import rimraf from 'rimraf';
 
 const $ = require('gulp-load-plugins')();
 
+function compileServer() {
+	return gulp.src('./src/server/**/*.js')
+		.pipe($.changed('./build'))
+		.pipe($.sourcemaps.init())
+		.pipe($.babel())
+		.pipe($.sourcemaps.write('.', { sourceRoot: path.join(__dirname, 'src', 'server') }))
+		.pipe(gulp.dest('./build'))
+		.on('error', () => {});
+}
+
+function watchServer() {
+	return gulp.watch('./src/server/**/*.js', compileServer)
+		.on('error', () => {});
+}
+
 gulp.task('server:clean', (cb) => {
 	rimraf('./build', () => cb());
 });
 
 gulp.task("server:build",
-	gulp.series('server:clean', () => {
-		return gulp.src('./src/server/**/*.js')
-			.pipe($.changed('./build'))
-			.pipe($.sourcemaps.init())
-			.pipe($.babel())
-			.pipe($.sourcemaps.write('.', { sourceRoot: path.join(__dirname, 'src', 'server') }))
-			.pipe(gulp.dest('./build'));
-		}
+	gulp.series('server:clean', compileServer));
+
+gulp.task('server:watch',
+	gulp.series('server:build', watchServer
+		));
+
+gulp.task('server:dev', gulp.series(
+	'server:build', gulp.parallel(
+		watchServer, function nodemon() {
+			$.nodemon({
+				script: './server.js',
+				watch: 'build'
+			});
+		})
 ));
--- src/server/server.js
+++ src/server/server.js
@@ -4,3 +4,4 @@ console.log(`hello ${bleh}`);
 const obj = { hey: 1 };
 const obj2 = { ...obj, bleh: 2 };
 console.log(obj2);
+console.log('meow');
